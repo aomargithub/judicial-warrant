7e0b9b9ee9db4773ec589b344ddce40e758b6ebe
aomargithub <aomar.github@gmail.com>
2018-03-12 20:56:01 +0400

    .

--- frontend/jwapp/src/generalConfig.js
+++ frontend/jwapp/src/generalConfig.js
@@ -1,4 +1,4 @@
- module.exports = function(app){
+module.exports = function(app){
     app.config(function($httpProvider){
         $httpProvider.interceptors.push('authenticationInterceptor');
     }).run(function($rootScope){
