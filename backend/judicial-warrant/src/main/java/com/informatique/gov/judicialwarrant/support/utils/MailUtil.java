ee0c8d3c529094935bf3aa969398ae63d15edb44
attabdullah <attabdullah@inf-05.Informatique.net>
2018-06-04 13:36:11 +0200

    user delete external

--- backend/judicial-warrant/src/main/java/com/informatique/gov/judicialwarrant/support/utils/MailUtil.java
+++ backend/judicial-warrant/src/main/java/com/informatique/gov/judicialwarrant/support/utils/MailUtil.java
@@ -1,31 +1,38 @@
 package com.informatique.gov.judicialwarrant.support.utils;
 
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
+
 @Component
 @AllArgsConstructor
 public class MailUtil {
 
+	private JavaMailSender  sender;
 
-	private MailSender  sender;
-	//Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	public  void send(String body,String recipient,String subject) throws Exception{
+	private void send(String body, String recipient, String subject) throws Exception {
+
+		  MimeMessage mail = sender.createMimeMessage();
+          MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+          helper.setTo(recipient);
+          helper.setSubject(subject);
+          helper.setText(body,true);
 		
-		        SimpleMailMessage mail = new SimpleMailMessage();
-		      //  mail.setFrom("attabdullah@informatique-eg.com");
-				mail.setTo(recipient);
-				mail.setSubject(subject);
-				mail.setText(body);
-				
-			//	logger.info("Sending...");
-				
-				sender.send(mail);
-				
-			//	logger.info("Done!");
-		
-		    }
+		sender.send(mail);
+
+	}
+
+	public void sendAccountCreation(String loginName, String password, String recipient) throws Exception{
+		String body = "<table style=\"width:100%\">\r\n" + "<caption><h2>" + loginName + "</h2></caption>\r\n"
+				+ "  <tr>\r\n" + "    <th>Dear Client ,<br/>\r\n" + "    We create account in judicial Warrant<br/>\r\n"
+				+ "    and your password is :" + password + "\r\n" + "    </th>\r\n"
+				+ "    <th>عزيزى العميل<br/> تم انشاء حساب فى الضبطية القضائية <br/>وكلمة السر :"+password+"</th> \r\n"
+				+ "   \r\n" + "  </tr>\r\n" + " \r\n" + "</table>";
+		send(body, recipient, "Creation Account in judical warrant");
+
+	}
 }
