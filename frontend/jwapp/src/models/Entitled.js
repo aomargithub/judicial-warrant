7582a1a6e6ede90ca64afdcbaa230cdad64cfc0d
ahmohamed <ahmohamed@PC-13.Informatique.net>
2018-07-03 08:54:52 +0200

    push missing files

---
+++ frontend/jwapp/src/models/Entitled.js
@@ -0,0 +1,21 @@
+module.exports = function(app){
+    app.factory('Entitled', function(EntitledRegistration){
+        return function Entitled(){
+            var self = this;
+            self.id = null;
+            self.englishName = null;
+            self.arabicName = null;
+            self.civilId = null;
+            self.mobileNumber1 = null;
+            self.mobileNumber1 = null;
+            self.mobileNumber2 = null;
+            self.emailAddress = null;
+            self.entitledRegistration = new EntitledRegistration();
+            self.organizationUnit = null;
+            self.currentStatusid = null;
+            self.id = null;
+            
+            
+        }
+    });
+};
