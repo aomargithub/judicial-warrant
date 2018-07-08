1cfd488df9db8d3c92bf48b90d2b907776c2dee6
Diaa Eldin Khalaf Hammad <Diaa Eldin Khalaf Hammad@informatique.net>
2018-07-08 13:28:59 +0200

    reload page after submit

--- frontend/jwapp/src/directives/entitledRegistration/entitledRegistrationDrtvCtrl.js
+++ frontend/jwapp/src/directives/entitledRegistration/entitledRegistrationDrtvCtrl.js
@@ -153,7 +153,7 @@ module.exports = function (app) {
         vm.reLoad = function() {
             // set serial in url to make user can refresh page and with same data
             // and refetch data in entitleds two change status
-            return $state.go('root.ENTITLED_REGISTRATION',{serial:vm.serial});
+            return $state.go('root.ENTITLED_REGISTRATION',{serial:vm.serial},{reload: true});
         }
 
     });
