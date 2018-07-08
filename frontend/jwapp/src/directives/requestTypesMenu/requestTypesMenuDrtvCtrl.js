f43670353b3d82bde756d80f4822024783f75a64
ahmohamed <ahmohamed@PC-13.Informatique.net>
2018-07-08 16:55:55 +0200

    solve home page request type with work around

--- frontend/jwapp/src/directives/requestTypesMenu/requestTypesMenuDrtvCtrl.js
+++ frontend/jwapp/src/directives/requestTypesMenu/requestTypesMenuDrtvCtrl.js
@@ -1,9 +1,21 @@
 module.exports = function(app){
-    app.controller('requestTypesMenuDrtvCtrl', function(requestTypeSrvc){
+    app.controller('requestTypesMenuDrtvCtrl', function(requestTypeSrvc, appSessionSrvc){
         var vm = this;
+        vm.items = [];
+        vm.role = appSessionSrvc.getCurrentUser().role;
 
-        requestTypeSrvc.getActiveByCurrentUserRole().then(function(response){
-            vm.items = response.data;
+        requestTypeSrvc.getAll().then(function(response){
+        if(vm.role === 'ROLE_OFFICER' || vm.role === 'ROLE_USER') {
+            response.data.forEach(function(rt, index) {
+                if(rt.code === 'CAPACITY_DELEGATION' && vm.role === 'ROLE_OFFICER') {
+                        vm.items.push(rt);
+                }
+                if(rt.code === 'ENTITLED_REGISTRATION' && vm.role === 'ROLE_USER') {
+                    vm.items.push(rt);
+                }  
+            });
+        }
+
         });
     });
 };
